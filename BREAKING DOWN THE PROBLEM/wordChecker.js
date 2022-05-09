ca6e6fe06b004c6c9502e03e866b924b37c5d9e0
BRUNO MOREIRA <brunomoreiramg@hotmail.com>
2022-05-08 21:37:44 -0300

    first commit

---
+++ BREAKING DOWN THE PROBLEM/wordChecker.js
@@ -0,0 +1,27 @@
+import { randomWord } from 'cross.words';
+
+const _words = ['bait', 'cart', 'cast', 'daft', 'dart', 'east', 'fact', 'fast', 'gait', 'gast', 'halt', 'last', 'malt', 'mast', 'pact', 'pant', 'part', 'past', 'raft', 'rant', 'salt', 'tact', 'tart', 'vast', 'waft', 'wait', 'want', 'wart', 'watt', 'cats', 'bats', 'rats', 'bate', 'bath', 'bats', 'cats', 'data', 'date', 'eats', 'fate', 'fats', 'gate', 'hats', 'late', 'mate', 'math', 'oath', 'oats', 'rate', 'rats', 'baits', 'darts', 'attach', 'attack', 'attain', 'strata', 'tartan', 'tartar', 'tatars', 'aeronautics', 'alterations', 'astronautic', 'automations', 'devastation', 'evacuations', 'evaluations', 'salutations', 'situational', 'accusation', 'aeronautic', 'agitations', 'alteration', 'automating', 'automation', 'gelatinous', 'hesitation', 'infatuated', 'passionate', 'poinsettia', 'quotations', 'restaurant', 'valuations', 'adult', 'basil', 'bones', 'doses', 'early', 'heads'];
+
+defineModule(
+  'cross.words',
+  {
+    randomSpace: pickRandom(_words).split('').map(i=>pickRandom([i,'-'])).join(''),
+    randomWord: pickRandom(_words)
+  }
+);
+
+
+function check(space, word, ___) {
+  if (space.length !== word.length) {
+    return false;
+  }
+  for (let i = 0; i < space.length; i++) {
+    if (space[i] !== '-' && space[i] !== word[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+console.log('-a-t');
+console.log(randomWord);
+console.log(check('-a-t', randomWord));

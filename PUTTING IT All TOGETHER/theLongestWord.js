356ee2d2bea52d69602309d9927618bccb1a3f36
BRUNO MOREIRA <brunomoreiramg@hotmail.com>
2022-05-12 22:53:02 -0300

    committing

---
+++ PUTTING IT All TOGETHER/theLongestWord.js
@@ -0,0 +1,15 @@
+import { subsequences, stringSequence };
+    
+let subsequences = ['able', 'ale', 'apple'];
+let stringSequence = ['abppplee'];
+
+
+function longestWord(array) {
+    let longeststring = "";
+
+    for (const word of array) {
+        if(word.length > longeststring.length)
+        longestWord = word;
+    }
+        return longeststring;
+}
